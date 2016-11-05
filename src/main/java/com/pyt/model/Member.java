a8dba99c26f2e3c9c4dbedd8de55201386d44f39
simone <simonematteostefanoriccardo@gmail.com>
2016-11-05 21:24:13 +0100

    setto il datasource con jdbc; imposto liquibase

--- src/main/java/com/pyt/model/Member.java
+++ src/main/java/com/pyt/model/Member.java
@@ -21,42 +21,39 @@ import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
-import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 
-import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 @XmlRootElement
-@Table(name = "Member_pyt", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+@Table(name = "Member", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 public class Member implements Serializable {
     /** Default value included to remove warning. Remove or modify at will. **/
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull
-    @Size(min = 1, max = 25, message = "1-25 letters and spaces")
-    @Pattern(regexp = "[^0-9]*", message = "Must not contain numbers")
+    //@Size(min = 1, max = 25, message = "1-25 letters and spaces")
+    //@Pattern(regexp = "[^0-9]*", message = "Must not contain numbers")
     private String name;
 
     @NotNull
     @NotEmpty
-    @Email(message = "Invalid format")
+    //@Email(message = "Invalid format")
     private String email;
 
     @NotNull
-    @Size(min = 10, max = 12, message = "10-12 Numbers")
-    @Digits(fraction = 0, integer = 12, message = "Not valid")
+    //@Size(min = 10, max = 12, message = "10-12 Numbers")
+    //@Digits(fraction = 0, integer = 12, message = "Not valid")
     @Column(name = "phone_number")
     private String phoneNumber;
 
