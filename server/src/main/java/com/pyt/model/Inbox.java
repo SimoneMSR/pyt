3a7ee8450418459cb788a119ce9ce1ff083edd30
Simone D'Amario <simone.damario@kalpa.it>
2017-01-09 15:55:40 +0100

    creo tabelle/entity per Mail

---
+++ server/src/main/java/com/pyt/model/Inbox.java
@@ -0,0 +1,56 @@
+package com.pyt.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.pyt.model.utils.InboxId;
+
+@Entity
+@XmlRootElement
+@Table(name = "Inbox")
+@IdClass(InboxId.class)
+public class Inbox implements Serializable{
+	
+	/** Default value included to remove warning. Remove or modify at will. **/
+    private static final long serialVersionUID = 1L;
+    
+     @Id
+     private int messageId;
+    
+     @Id
+     private int receiverId;
+     
+     private Date deleted;
+
+	public int getMessageId() {
+		return messageId;
+	}
+
+	public void setMessageId(int messageId) {
+		this.messageId = messageId;
+	}
+
+	public int getReceiverId() {
+		return receiverId;
+	}
+
+	public void setReceiverId(int receiverId) {
+		this.receiverId = receiverId;
+	}
+
+	public Date getDeleted() {
+		return deleted;
+	}
+
+	public void setDeleted(Date deleted) {
+		this.deleted = deleted;
+	}
+     
+     
+}
---
+++ server/src/main/java/com/pyt/model/Message.java
@@ -0,0 +1,97 @@
+package com.pyt.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@XmlRootElement
+@Table(name = "Message")
+public class Message implements Serializable{
+
+	/** Default value included to remove warning. Remove or modify at will. **/
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idMessage;
+    
+	@ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="creatorId", updatable=false)
+    private Member creator;
+	
+	@ManyToMany(fetch=FetchType.LAZY)
+	@JoinTable(name="MessageRecipient",
+	        joinColumns=@JoinColumn(name="messageId"),
+	        inverseJoinColumns=@JoinColumn(name="recipientId"))
+	private Set<Member> recipients;
+    
+    @NotNull
+    private String subject;
+    
+    private String object;
+    
+    @NotNull
+    private Date date;
+
+	public Member getCreator() {
+		return creator;
+	}
+
+	public void setCreator(Member creator) {
+		this.creator = creator;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getObject() {
+		return object;
+	}
+
+	public void setObject(String object) {
+		this.object = object;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public int getIdMessage() {
+		return idMessage;
+	}
+
+	public Set<Member> getRecipients() {
+		return recipients;
+	}
+
+	public void setRecipients(Set<Member> recipients) {
+		this.recipients = recipients;
+	}
+	
+	
+    
+    
+}
